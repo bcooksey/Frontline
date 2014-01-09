7c11725b905b16a9fd5af386ba0f021187af8696
Brian Cooksey <bac2k@hotmail.com>
2014-01-09 00:36:43 -0600

    First stab at breadth-first search for move

--- engine/zone.go
+++ engine/zone.go
@@ -1,6 +1,11 @@
 package engine
 
+import "container/heap"
+import "fmt"
+var _ = fmt.Println
+
 type Zone struct {
+    id   int
     occupyingUnits   []Unit
     neighboringZones []Zone
     supplyValue      int
@@ -20,4 +25,55 @@ func (z *Zone) ControllingPower() string { return z.controllingPower }
 func (z *Zone) AddOccupyingUnit(newUnit Unit) bool {
     z.occupyingUnits = append(z.occupyingUnits, newUnit)
     return true
+}
+
+type ZoneQueue []Zone
+
+func (q ZoneQueue) Len() int           { return len(q) }
+func (q ZoneQueue) Less(i, j int) bool { panic("Do not run less") }
+func (q ZoneQueue) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
+
+func (q *ZoneQueue) Push(z interface{}) {
+	*q = append(*q, z.(Zone))
+}
+
+func (q *ZoneQueue) Pop() interface{} {
+	old := *q
+	n := len(old)
+	z := old[n-1]
+	*q = old[0 : n-1]
+	return z
+}
+
+func Move(fromZone Zone, toZone Zone, unit Unit) bool {
+    if fromZone.id == toZone.id {
+        return true
+    }
+
+    queue := &ZoneQueue{}
+    visits := map[int]bool{}
+
+    heap.Init(queue)
+
+    visits[fromZone.id] = true
+    queue.Push(fromZone)
+
+    // Do a breadth-first search to see if the zones connect
+    for queue.Len() > 0 {
+        fmt.Println("************\n")
+        currentZone := queue.Pop().(Zone)
+        fmt.Printf("Looking at %d\n", currentZone.id)
+        for _, neighbor := range currentZone.NeighboringZones() {
+            fmt.Printf("  - Neighbor %d\n", neighbor.id)
+            if !visits[neighbor.id] {
+                if neighbor.id == toZone.id {
+                    return true
+                } else {
+                    visits[neighbor.id] = true
+                    queue.Push(neighbor)
+                }
+            }
+        }
+    }
+    return false
 }
--- engine/zone_test.go
+++ engine/zone_test.go
@@ -1,6 +1,8 @@
 package engine
 
 import "testing"
+import "fmt"
+var _ = fmt.Println
 
 func TestZoneGetter(t *testing.T) {
     zone := Zone{supplyValue: 2}
@@ -19,4 +21,35 @@ func TestAddUnitToZone(t *testing.T) {
     if len(zone.OccupyingUnits()) != 1 {
         t.Error("AddOccupyingUnit did not add unit to zone")
     }
+}
+
+func TestMoveUnit(t *testing.T) {
+    zone1 := Zone{id:1}
+    zone2 := Zone{id:2, neighboringZones: []Zone{zone1}}
+    zone4 := Zone{id: 4}
+    zone3 := Zone{id: 3, neighboringZones: []Zone{zone1, zone4}}
+    zone1.neighboringZones = []Zone{zone2, zone3}
+
+    for _, z := range zone1.neighboringZones[1].neighboringZones {
+        fmt.Println(z.id)
+    }
+
+    unit := Unit{movementRange: 3}
+
+    if Move(zone1, zone1, unit) != true {
+        t.Error("Did not claim keeping a unit in same zone is valid")
+    }
+
+    if Move(zone1, zone4, unit) != true {
+        t.Error("Could not successfully move a unit from one zone to another")
+    }
+
+    /* Cases to test:
+     *   Zone is beyond the units movement capability
+     *   One path is beyond units movement, another is ok
+     *   Land unit trying to go through sea
+     *   Land unit having to go through impassible area
+     *   Sea units
+     *   Air units
+     */
 }
